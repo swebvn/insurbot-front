40cc2a4d697ee2792ff134a0b8645bd0d1f730b9
Deedevv <dokhacde1903@gmail.com>
2025-05-14 12:49:09 +0700

    main

--- src/echo.js
+++ src/echo.js
@@ -7,6 +7,7 @@ const echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     wsHost: import.meta.env.VITE_WSBHOST,
+    wsPath: '/app',
     cluster: '',
     wsPort: 6001,
     forceTLS: false,
